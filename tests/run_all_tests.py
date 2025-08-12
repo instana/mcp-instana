e083b7f92341fed7a8d18d303fe6db58bea5c934
Riya Kumari <Riya.Kumari3@ibm.com>
2025-08-12 12:18:36 +0530

    removed prompts tests

--- tests/run_all_tests.py
+++ tests/run_all_tests.py
@@ -295,8 +295,7 @@ def run_all_tests(test_path: Optional[str] = None, verbose: bool = False) -> boo
             'tests.infrastructure.test_infrastructure_metrics',
             'tests.infrastructure.test_infrastructure_resources',
             'tests.infrastructure.test_infrastructure_topology',
-            'tests.log.test_log_alert_configuration',
-            'tests.prompts.test_mcp_prompts'
+            'tests.log.test_log_alert_configuration'
         ]
 
         all_tests = unittest.TestSuite()
