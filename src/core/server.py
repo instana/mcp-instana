867b1b4a80e8f1e9419b8b83739473ace373f3e2
Riya Kumari <Riya.Kumari3@ibm.com>
2025-08-13 19:35:35 +0530

    application test

--- src/core/server.py
+++ src/core/server.py
@@ -187,8 +187,8 @@ def get_client_categories():
         from src.application.application_catalog import ApplicationCatalogMCPTools
         from src.application.application_metrics import ApplicationMetricsMCPTools
         from src.application.application_resources import ApplicationResourcesMCPTools
-        from src.application.application_topology import ApplicationTopologyMCPTools
         from src.application.application_settings import ApplicationSettingsMCPTools
+        from src.application.application_topology import ApplicationTopologyMCPTools
         from src.event.events_tools import AgentMonitoringEventsMCPTools
         from src.infrastructure.infrastructure_analyze import (
             InfrastructureAnalyzeMCPTools,
